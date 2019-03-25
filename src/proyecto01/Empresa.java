be09bb67cbad5b2ef6e73ea86b43310b28086a20
Thomas <thomascaycedo19@outlook.com>
2019-03-25 03:00:39 -0500

    Se crean las vistas del cliente y empresa

---
+++ src/proyecto01/Empresa.java
@@ -0,0 +1,66 @@
+
+package proyecto01;
+
+public class Empresa {
+    
+    private String Nit;
+    private String Nombre;
+    private String Direccion;
+    private String Telefono;
+
+    /**
+     * @return the Nit
+     */
+    public String getNit() {
+        return Nit;
+    }
+
+    /**
+     * @param Nit the Nit to set
+     */
+    public void setNit(String Nit) {
+        this.Nit = Nit;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Direccion
+     */
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    /**
+     * @param Direccion the Direccion to set
+     */
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    /**
+     * @return the Telefono
+     */
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    /**
+     * @param Telefono the Telefono to set
+     */
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+}
---
+++ src/proyecto01/Proyecto01.java
@@ -0,0 +1,15 @@
+
+package proyecto01;
+
+public class Proyecto01 {
+
+
+    public static void main(String[] args) {
+
+        
+        
+        
+        
+    }
+    
+}
