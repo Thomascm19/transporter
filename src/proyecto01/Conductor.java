ab02bc89ec3e3de6e23dbb07ab9bf9e2c6cae44a
Felipe Pineda <henaopipepineda@gmail.com>
2019-04-03 14:49:43 -0500

    Se actulizo la clase conductor

--- src/proyecto01/Conductor.java
+++ src/proyecto01/Conductor.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto01;
 
 /**
  *
  * @author thoma
  */
-public class Conductor {
+public class Conductor extends Persona{
+    
+    private int NumeroLicencia;
+    private String Experiencia;
+    private int CodigoEmpresa;
+
+    /**
+     * @return the NumeroLicencia
+     */
+    public int getNumeroLicencia() {
+        return NumeroLicencia;
+    }
+
+    /**
+     * @param NumeroLicencia the NumeroLicencia to set
+     */
+    public void setNumeroLicencia(int NumeroLicencia) {
+        this.NumeroLicencia = NumeroLicencia;
+    }
+
+    /**
+     * @return the Experiencia
+     */
+    public String getExperiencia() {
+        return Experiencia;
+    }
+
+    /**
+     * @param Experiencia the Experiencia to set
+     */
+    public void setExperiencia(String Experiencia) {
+        this.Experiencia = Experiencia;
+    }
+
+    /**
+     * @return the CodigoEmpresa
+     */
+    public int getCodigoEmpresa() {
+        return CodigoEmpresa;
+    }
+
+    /**
+     * @param CodigoEmpresa the CodigoEmpresa to set
+     */
+    public void setCodigoEmpresa(int CodigoEmpresa) {
+        this.CodigoEmpresa = CodigoEmpresa;
+    }
+    
+    
+    
     
 }
